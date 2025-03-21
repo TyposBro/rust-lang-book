0c265336b87069ebde0bf8a6814cd0219f2bc57d
typosbro <typosbro@proton.me>
2025-03-22 02:30:51 +0900

    guessing game

--- _03_guessing_game/src/main.rs
+++ _03_guessing_game/src/main.rs
@@ -1,14 +1,33 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    let select = rand::rng().random_range(1..=100);
     println!("Guess the number");
-    println!("Please input number:");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input number:");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    print!("You guessed: {}", guess)
+        print!("You guessed: {guess}");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&select) {
+            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+    }
 }
